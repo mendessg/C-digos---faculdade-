2500e5d457cb0efc6581848d6af9a5a88b5658bf
lais <laismendesg11@gmail.com>
2026-09-08 21:18:06 -0300

    C-digos---faculdade-/media_simples.py

---
+++ media_simples.py
@@ -0,0 +1,6 @@
+n1= int(input("Escreva a 1 nota: "))
+n2= int(input("Escreva a 2 nota: "))
+n3= int(input("Escreva a 3 nota: "))
+n4= int(input("Escreva a 4 nota: "))
+media= n1 + n2 + n3 + n4 / 4 
+print("Valor total da media: ", media)
